2d19ae8a4fd2615f2f8110c61ed07ee51736e3f1
vivianeendler <vdendler@inf.ufsm.br>
2022-06-09 13:38:23 -0300

    Adicionando fragmento pt-3
    box central

---
+++ liferay-workspace/fragments/treinamento/src/vde-collection/pt-3-box-central/main.js
@@ -0,0 +1,34 @@
+// import React from 'react';
+
+// export default function (props) {
+//   console.log("props", props);
+
+//   return <h1>Hello world</h1>;
+// }
+const slide = fragmentElement.querySelector('.slide');
+const indicators = Array.from(
+	fragmentElement.querySelectorAll('.carousel-indicators > li')
+);
+const carouselControls = Array.from(
+	fragmentElement.querySelectorAll(
+		'.carousel-control-prev, .carousel-control-next'
+	)
+);
+const carouselId = fragmentElement.id + '-carousel';
+
+slide.id = carouselId;
+indicators.forEach(function(indicator) {
+	indicator.dataset.target = '#' + carouselId;
+});
+carouselControls.forEach(function(control) {
+	control.href = '#' + carouselId;
+});
+
+slide.classList.add('carousel');
+
+if (document.querySelector('.has-edit-mode-menu')) {
+	$(slide).carousel('pause');
+	$('.carousel').off('keydown.bs.carousel');
+} else {
+	$(slide).carousel({ride: 'carousel'});
+}
