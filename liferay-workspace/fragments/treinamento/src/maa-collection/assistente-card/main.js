638dde8879195ad07904689469a227944e30f66f
Marianna Alves Angelos <maa@intranet.pitang.com>
2022-06-09 13:56:42 -0300

    add js

--- liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
+++ liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
@@ -2,3 +2,19 @@ console.group('assistente-card');
 console.log('fragmentElement', fragmentElement);
 console.log('configuration', configuration);
 console.groupEnd();
+
+var tabs = document.querySelectorAll('.dot'), 
+contents = document.querySelectorAll('.assistente-card-right');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabs.forEach((tab)=>{
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabs[index].classList.add('is-active');
+    });
+});
