58d91c7a96e3183004eeafeb41b21613780846f6
Marianna Alves Angelos <maa@intranet.pitang.com>
2022-06-10 13:14:44 -0300

    add js to title element

--- liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
+++ liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
@@ -4,7 +4,8 @@ console.log('configuration', configuration);
 console.groupEnd();
 
 var tabs = document.querySelectorAll('.step'), 
-contents = document.querySelectorAll('.right-bottom-element');
+contents = document.querySelectorAll('.right-bottom-element'),
+titles = document.querySelectorAll('.right-top-element');
 
 tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
@@ -14,7 +15,11 @@ tabs.forEach((tab, index) => {
         tabs.forEach((tab)=>{
             tab.classList.remove('is-active');
         });
+        titles.forEach((title)=>{
+            title.classList.remove('is-active');
+        });
         contents[index].classList.add('is-active');
         tabs[index].classList.add('is-active');
+        titles[index].classList.add('is-active');
     });
 });
