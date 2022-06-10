5d9e16f6ff2f8c4073aa02d646aa2ec4e6583362
Marianna Alves Angelos <maa@intranet.pitang.com>
2022-06-10 16:37:42 -0300

    add step bottom

--- liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
+++ liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
@@ -3,23 +3,23 @@ console.log('fragmentElement', fragmentElement);
 console.log('configuration', configuration);
 console.groupEnd();
 
-var tabs = document.querySelectorAll('.step'), 
+var stepsTop = document.querySelectorAll('.step-top'), 
 contents = document.querySelectorAll('.right-bottom-element'),
 titles = document.querySelectorAll('.right-top-element');
 
-tabs.forEach((tab, index) => {
+stepsTop.forEach((tab, index) => {
     tab.addEventListener('click', () => {
         contents.forEach((content) => {
             content.classList.remove('is-active');
         });
-        tabs.forEach((tab)=>{
+        stepsTop.forEach((tab)=>{
             tab.classList.remove('is-active');
         });
         titles.forEach((title)=>{
             title.classList.remove('is-active');
         });
         contents[index].classList.add('is-active');
-        tabs[index].classList.add('is-active');
+        stepsTop[index].classList.add('is-active');
         titles[index].classList.add('is-active');
     });
 });
