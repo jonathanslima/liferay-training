489b5457a92cb943a1816486eb892ad9349841e9
Marianna Alves Angelos <maa@intranet.pitang.com>
2022-06-13 09:12:53 -0300

    add js card interno

--- liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
+++ liferay-workspace/fragments/treinamento/src/maa-collection/assistente-card/main.js
@@ -4,7 +4,7 @@ console.log('configuration', configuration);
 console.groupEnd();
 
 var stepsTop = document.querySelectorAll('.step-top'), 
-contents = document.querySelectorAll('.right-bottom-element'),
+contents = document.querySelectorAll('.right-bottom-element-parent'),
 titles = document.querySelectorAll('.right-top-element');
 
 stepsTop.forEach((tab, index) => {
@@ -23,3 +23,20 @@ stepsTop.forEach((tab, index) => {
         titles[index].classList.add('is-active');
     });
 });
+
+var stepsBottomFive = document.querySelectorAll('.step-bottom-child.five'),
+contentsFive = document.querySelectorAll('.right-bottom-element-child.five');
+
+stepsBottomFive.forEach((step,index) => {
+    step.addEventListener('click', () => {
+        contentsFive.forEach((contentFive) => {
+            contentFive.classList.remove('is-active');
+        });
+        stepsBottomFive.forEach((stepFive) => {
+            stepFive.classList.remove('is-active');
+        });
+        contentsFive[index].classList.add('is-active');
+        stepsBottomFive[index].classList.add('is-active');
+    });
+});
+
