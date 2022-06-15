b5475324bd57ad78275e426acfbec7937704c855
renatoLoureiroLima <renatoloureiro45@gmail.com>
2022-06-15 13:41:41 -0300

    Fragmento parcialmente completo e funcionando, bug apenas na parte dos anterior e proximo

--- liferay-workspace/fragments/treinamento/src/rll-collection/etapas-abas-avatar/main.js
+++ liferay-workspace/fragments/treinamento/src/rll-collection/etapas-abas-avatar/main.js
@@ -2,6 +2,9 @@ console.group('Title-Labs');
 console.log('fragmentElement', fragmentElement);
 console.log('configuration', configuration);
 console.groupEnd();
+
+//segunda função
+//pegando as classes dos botões circulates
 let buttons_circle = document.querySelectorAll(".btn-item");
 let contents = document.querySelectorAll(".content-info-botom-personagem");
 
@@ -10,10 +13,51 @@ buttons_circle.forEach((tab, index) => {
 		contents.forEach((content) => {
 		content.classList.remove("is-active");
 		});
+
 		buttons_circle.forEach((tab) => {
 			tab.classList.remove("ativado");
 		});
 		contents[index].classList.add("is-active");
 		buttons_circle[index].classList.add("ativado");
 	});										 
+});
+//segunda função
+// pegando as classes dos tracinhos dentro do conteúdo
+let itens_stroke = document.querySelectorAll(".item-tirth-stroke");
+itens_stroke.forEach((itemTab, i) => {
+    itemTab.addEventListener("click", () =>{
+
+        //irá remover a classe is-active de todos outros itens, exceto o clicado
+        contents.forEach((content) => {
+		content.classList.remove("is-active");
+		});
+        //removendo a classe de todos os outros botões, exceto o que foi clicado
+        itens_stroke.forEach((tab) => {
+			tab.classList.remove("btn-ativo"); 
+		});
+        //sempre irá adicionar a classe btn-ativo na posição que for clicada
+        itens_stroke[i].classList.add("btn-ativo");
+        //sempre irá adicionar a classe ativa ao item que for clicado
+        contents[i].classList.add("is-active");
+    });
+    
+});
+//terceira função
+let buttons_bottom = document.querySelectorAll(".buttons-bottom");
+
+buttons_bottom.forEach((botao, i) =>{
+    botao.addEventListener("click", () =>{
+        
+        contents.forEach((content) => {
+		content.classList.remove("is-active");
+		});
+        //removendo a classe de todos os outros botões, exceto o que foi clicado
+        itens_stroke.forEach((tab) => {
+			tab.classList.remove("btn-ativo"); 
+		});
+        //sempre irá adicionar a classe btn-ativo na posição que for clicada
+        itens_stroke[i].classList.add("btn-ativo");
+        //sempre irá adicionar a classe ativa ao item que for clicado
+        contents[i].classList.add("is-active");
+    });
 });
